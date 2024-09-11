3f89d9769cf9062cecd89e23873c2e4bf4200fd8
AArvidd <arvid.r.oscarsson@gmail.com>
2024-09-11 10:43:44 +0200

    added algoritmem to revove to small caves from map

--- main.c
+++ main.c
@@ -1,15 +1,97 @@
 #include "raylib.h"
 #include "stdio.h"
 #include "time.h"
+// #include "stdlib.h"
 
-#define width 100
-#define height 100
+#define width 80
+#define height 90
+
+#define queue_lenth 200
+
+#define search_cross 4
+#define seatch_full 8
+
+typedef struct{
+    int curent;
+    int next_empty;
+    int queue_x[queue_lenth];
+    int queue_y[queue_lenth];
+}queue_t;
 
 int map1[width][height];
 int map2[width][height];
+int map3[width][height];
 
-int spatern_x[] = {-1, -1,  0,  1,  1,  1,  0, -1};
-int spatern_y[] = { 0, -1, -1, -1,  0,  1,  1,  1};
+int spatern_x[] = {-1,  1,  0,  0, -1,  1, -1,  1};
+int spatern_y[] = { 0,  0, -1,  1, -1, -1,  1,  1};
+
+
+queue_t queue = {
+    .curent = 0,
+    .next_empty = 0
+};
+
+
+int flood_search(int x, int y){
+    int area = 1;
+    queue.queue_x[0] = x;
+    queue.queue_y[0] = y;
+    queue.next_empty++;
+    map2[x][y] = 1;
+    while(queue.curent != queue.next_empty){
+        for(int i = 0; i < search_cross; i++){
+            if(map1[queue.queue_x[queue.curent] + spatern_x[i]][queue.queue_y[queue.curent] + spatern_y[i]] == 0 && map2[queue.queue_x[queue.curent] + spatern_x[i]][queue.queue_y[queue.curent] + spatern_y[i]] != 1){
+                map2[queue.queue_x[queue.curent] + spatern_x[i]][queue.queue_y[queue.curent] + spatern_y[i]] = 1;
+                area++;
+                queue.queue_x[queue.next_empty] = queue.queue_x[queue.curent] + spatern_x[i];
+                queue.queue_y[queue.next_empty] = queue.queue_y[queue.curent] + spatern_y[i];
+                queue.next_empty++;
+                if(queue.next_empty == queue_lenth){
+                    queue.next_empty = 0;
+                }
+
+            }
+        }
+        queue.queue_x[queue.curent] = 0;
+        queue.queue_y[queue.curent] = 0;
+        queue.curent++;
+        if(queue.curent == queue_lenth){
+            queue.curent = 0;
+        }
+    }
+    queue.curent = 0;
+    queue.next_empty = 0;
+    return area;
+}
+
+void flood_remove(int x, int y){
+    queue.queue_x[0] = x;
+    queue.queue_y[0] = y;
+    queue.next_empty++;
+    map1[x][y] = 1;
+    while(queue.curent != queue.next_empty){
+        for(int i = 0; i < search_cross; i++){
+            if(map1[queue.queue_x[queue.curent] + spatern_x[i]][queue.queue_y[queue.curent] + spatern_y[i]] == 0){
+                map1[queue.queue_x[queue.curent] + spatern_x[i]][queue.queue_y[queue.curent] + spatern_y[i]] = 1;
+                queue.queue_x[queue.next_empty] = queue.queue_x[queue.curent] + spatern_x[i];
+                queue.queue_y[queue.next_empty] = queue.queue_y[queue.curent] + spatern_y[i];
+                queue.next_empty++;
+                if(queue.next_empty == queue_lenth){
+                    queue.next_empty = 0;
+                }
+
+            }
+        }
+        queue.queue_x[queue.curent] = 0;
+        queue.queue_y[queue.curent] = 0;
+        queue.curent++;
+        if(queue.curent == queue_lenth){
+            queue.curent = 0;
+        }
+    }
+    queue.curent = 0;
+    queue.next_empty = 0;
+}
 
 void map_gen(){
     for(int x = 0; x < width; x++){
@@ -28,8 +110,8 @@ void map_gen(){
             for(int y = 0; y < height; y++){
 
                 int neighbors = 0;
-                for(int i = 0; i < 8; i++){
-                    if(x + spatern_x[i] < 0 || x + spatern_x[i] >= width || y + spatern_y[i] < 0 || y + spatern_y[i] >= width){
+                for(int i = 0; i < seatch_full; i++){
+                    if(x + spatern_x[i] < 0 || x + spatern_x[i] >= width || y + spatern_y[i] < 0 || y + spatern_y[i] >= height){
                         neighbors++;
                     }else if(map1[x + spatern_x[i]][y + spatern_y[i]]){
                         neighbors++;
@@ -51,7 +133,33 @@ void map_gen(){
                 map1[x][y] = map2 [x][y];
             }
         }
+    }
 
+    for(int x = 0; x < width; x++){
+        for(int y = 0; y < height; y++){
+            map2[x][y] = 0;
+        }
+    }
+
+    int remove_x[20];
+    int remove_y[20];
+    int remove = 0;
+
+    for(int x = 0; x < width; x++){
+        for(int y = 0; y < height; y++){
+            if(map1[x][y] == 0 && map2[x][y] != 1){
+                int area = flood_search(x, y);
+                if(area < 11){
+                    remove_x[remove] = x;
+                    remove_y[remove] = y;
+                    remove++;
+                }
+            }
+        }
+    }
+
+    for(int i = 0; i < remove; i++){
+        flood_remove(remove_x[i], remove_y[i]);
     }
 }
 
@@ -73,7 +181,7 @@ int main(){
             if(map1[x][y] == 1){
                 printf(" # ");
             }else{
-                printf(" . ");
+                printf("   ");
             }
         }
         printf("\n");
